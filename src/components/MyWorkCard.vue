5ab8d629e8129fd74745669d1de3815d6a24dc73
Tofan Vlad <VladutuT@gmail.com>
2024-11-03 02:47:23 +0200

    feat: Card hover animation

--- src/components/MyWorkCard.vue
+++ src/components/MyWorkCard.vue
@@ -1,18 +1,46 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const container = ref<HTMLElement | null>(null);
 defineProps<{
   title: string;
   text: string;
   mainImage: string;
   secondImage: string;
 }>();
+
+onMounted(() => {
+  if (!container.value) return;
+
+  container.value.addEventListener("mousemove", (event) => {
+    container.value!.style.transitionDuration = "0ms";
+    const cursorX =
+      (event.clientX - container.value!.offsetLeft) /
+        (container.value!.offsetWidth / 2) -
+      1;
+
+    const YMove = (-container.value!.clientHeight / 2 + event.clientY) / 8;
+
+    container.value!.style.transform = `translate(${
+      cursorX * 25
+    }px, ${YMove}px)`;
+  });
+
+  container.value.addEventListener("mouseleave", () => {
+    container.value!.style.transition = "transform";
+    container.value!.style.transitionDuration = "600ms";
+    container.value!.style.transform = `translate(0,0)`;
+  });
+});
 </script>
 
 <template>
   <article
+    ref="container"
     :style="{
       'background-image': `url(${mainImage})`,
     }"
-    class="flex flex-col max-w-[45%] object-scale-down group [background-position:center] bg-no-repeat [background-size:125%] backdrop-blur-sm hover:[background-size:145%] duration-300 transition-all text-secondary"
+    class="flex flex-col transition-[background] duration-300 max-w-[45%] object-scale-down group [background-position:center] bg-no-repeat [background-size:125%] backdrop-blur-sm hover:[background-size:145%] text-secondary"
   >
     <div
       class="relative w-full h-full p-4 overflow-hidden transition-all duration-300 group-hover:backdrop-blur-lg"
@@ -39,9 +67,11 @@ defineProps<{
 article:hover > div > .noise-bg {
   opacity: 0.9;
 }
+
 article:hover * img {
   transform: translateY(0);
 }
+
 .noise-bg {
   position: absolute;
   top: -50%;
@@ -65,33 +95,43 @@ article:hover * img {
   0% {
     background-position: 0, 0;
   }
+
   10% {
     background-position: -5%, -5%;
   }
+
   20% {
     background-position: -10%, 5%;
   }
+
   30% {
     background-position: 5%, -10%;
   }
+
   40% {
     background-position: -5%, 15%;
   }
+
   50% {
     background-position: -10%, 5%;
   }
+
   60% {
     background-position: 15%, 0;
   }
+
   70% {
     background-position: 0, 10%;
   }
+
   80% {
     background-position: -15%, 0;
   }
+
   90% {
     background-position: 10%, 5%;
   }
+
   100% {
     background-position: 5%, 0;
   }
